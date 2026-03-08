ff766f73c5b8d1a1df1faa847e0c76cdb69d4a92
Shogun585 <abhi12344spidey@gmail.com>
2026-03-08 23:13:18 +0530

    updated : prisma.js, schemas in zodSchema.js

--- backend-sec/schemas/zodSchema.js
+++ backend-sec/schemas/zodSchema.js
@@ -4,3 +4,18 @@ export const loginSchema = z.object({
     username : z.string(),
     password : z.string()
 })
+
+export const signupSchema = z.object({
+    id : z.string(),
+    password : z.string(),
+    firstName : z.string(),
+    lastName : z.string(),
+    role : z.enum(["student", "warden", "guard", "other"]),
+    contactDetails : z.string().optional()
+})
+
+export const passCreateShema = z.object({
+    passType : z.enum(["market", "leave", "other"]),
+    leaveStart : z.date(),
+    leaveEnd : z.date()
+})
---
+++ backend-sec/utils/prisma.js
@@ -0,0 +1,5 @@
+import {PrismaClient} from "backend-sec/generated/prisma/client"
+
+const prisma = new PrismaClient()
+
+export default prisma
