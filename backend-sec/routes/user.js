52036d34859ab3edef543b438d018724fe0d5f5d
Shogun585 <abhi12344spidey@gmail.com>
2026-05-26 23:07:41 +0530

    added functionality to ensure admin-only, user creation & deletion

--- backend-sec/routes/user.js
+++ backend-sec/routes/user.js
@@ -2,10 +2,11 @@ const express = require('express');
 const prisma = require('../utils/database');
 const hash = require('../utils/hash');
 const { schemas, validate } = require('../utils/schemas');
+const { getCurrentUser, requireRole } = require('../utils/oauth2');
 
 const router = express.Router();
 
-router.post('/', validate(schemas.userCreate), async (req, res) => {
+router.post('/', getCurrentUser, requireRole('admin'),  validate(schemas.userCreate), async (req, res) => {
     try {
         const existingUser = await prisma.user.findUnique({ where: { id: req.body.id } });
         if (existingUser) {
@@ -27,5 +28,36 @@ router.post('/', validate(schemas.userCreate), async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 });
+
+router.delete('/:id', getCurrentUser, requireRole('admin'), async(req, res)=>{
+    const targetedUserId = req.params.id;
+
+    try{
+        const targetUser = await prisma.user.findUnique({
+            where : targetedUserId
+        });
+
+        if(!targetUser){
+            return res.status(404).json({
+                detail : `User with ${targetedUserId} not found`
+            })
+        }
+
+
+        if(targetUser.role.toLowerCase() === 'admin'){
+            return res.status(403).json({ 
+                detail: "Action forbidden: Deletion of admin accounts is strictly prohibited." 
+            });
+        }
+ 
+        await prisma.user.delete({ 
+            where: { id: targetUserId } 
+        });
+
+        res.json({ detail: `User ${targetUserId} successfully deleted` });
+    }catch(err){
+        res.status(500).json({ error: err.message });
+    }
+})
 
 module.exports = router;
